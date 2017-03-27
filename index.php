2ad8bfce3793edc84e7e18b2db346af4d4e294ce
Bryant Smith <bryant.robert.smith@gmail.com>
2017-03-27 18:25:26 -0500

    Add content for header and main section; Begin styling grid; Begin type styles;

--- index.php
+++ index.php
@@ -22,19 +22,56 @@
   </head>
   <body>
     <header class="site-header" role="banner">
-      <div class="wrap">
-        <h1>Trio Home Services</h1>
-      </div>
+      <h1>Trio Home Services</h1>
+      <nav>
+        <ul>
+          <li><a href="#what-we-do">What We Do</a></li>
+          <li><a href="#rates-and-scheduling">Rates and Scheduling</a></li>
+          <li><a href="#about-trio">About Trio</a></li>          
+        </ul>
+      </nav>
     </header>
     <main class="site-main" role="main">
-      <div class="wrap">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam velit, libero sit ea vero ullam nulla, reprehenderit iusto vel porro, earum quisquam commodi. Ipsam atque sequi provident reprehenderit aspernatur magni.</p>
-      </div>
+      <section class="site-section bigger-text">
+        <div class="section-content">
+          <p>Compassionate, professional and personal assistance in the comfort of your own home.</p>
+          <p>Trio is a family business that offers comprehensive home services, customized to meet each one of our client’s needs.</p>
+        </div>
+      </section>
+      <section class="site-section" id="what-we-do">
+        <h2 class="section-title">What We Do</h2>
+        <div class="section-content">
+          <p>We are Certified Nursing Assistants with decades of combined experience in housekeeping, personal care and case management, with a focus on assisting seniors and individuals with disabilities. </p>
+          <h3>We do it all:</h3>
+          <ul>
+            <li>Housekeeping</li>
+            <li>Personal Care &amp; Errands</li>
+            <li>Pet Care</li>
+            <li>Mobility Assistance</li>
+          </ul>
+          <p>Our number one priority is ensuring our clients are healthy and safe, with all their needs provided for.</p>
+        </div>
+      </section>
+      <section class="site-section" id="rates-and-scheduling">
+        <h2 class="section-title">Rates And Scheduling</h2>
+        <div class="section-content">
+          <p>We are Certified Nursing Assistants with decades of combined experience in housekeeping, personal care and case management, with a focus on assisting seniors and individuals with disabilities.</p>
+          <p>Our rates start at $15/hour, for a minimum of three hours. It’s important to us that those who need accessible, affordable assistance can find it. Call or email to get a quote.</p>
+          <p>Whether it’s two hours a week, daily assistance or one full day of deep cleaning and comprehensive care, we’re committed to delivering excellent, kind and reliable service.</p>
+          <a href="mailto:" class="button">Email us to get a quote</a>
+          <p class="centered">Or call 312.434.0083</p>
+        </div>
+      </section>
+      <section class="site-section" id="about-trio">
+        <h2 class="section-title">About Trio</h2>
+        <div class="section-content">
+          <p>We have extensive experience in case management, home care and nursing assistance, with credentials that include Heartland Alliance, Casa Norte and NextLevelHealth. We are compassionate communicators and natural helpers who understand the difficulty many people, especially seniors, face trying to navigate a complex healthcare system and find the support they need. </p>
+          <p>Trio was founded by Chapearl Thompson, CNA, a Chicago native and proud member of her local community, with ties to Humboldt Park organizations Reason to Give and Firebelly Design. She brought her sister and daughter on board to round out the family business in 2017.</p>
+        </div>
+      </section>
     </main>
     <footer class="site-footer" role="contentinfo">
-      <div class="wrap">
-        <p class="copyright">©<?= date("Y"); ?> Trio Home Services</p>
-      </div>
+
     </footer>
     <script src="assets/js/build/main.min.js"></script>
     <script type="text/javascript">// Google Analytics
